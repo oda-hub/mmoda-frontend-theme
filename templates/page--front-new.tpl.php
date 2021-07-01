0bf6185423069b6eb0de156117d3c01a0c85e917
Mohamed Meharga <mohamed.meharga@unige.ch>
2021-07-01 17:26:36 +0200

    Add logos and a JS file to fix modal issues

--- templates/page--front-new.tpl.php
+++ templates/page--front-new.tpl.php
@@ -73,101 +73,16 @@
  *
  * @ingroup templates
  */
-
-if ($is_front) {
-  $page['content']['system_main']['default_message'] = array(); // This will remove the 'No front page content has been created yet.'
-}
+ if($is_front){
+     $page['content']['system_main']['default_message'] = array(); // This will remove the 'No front page content has been created yet.'
+  }
 ?>
-<header id="navbar" role="banner"
-  class="<?php print $navbar_classes; ?>">
-  <div class="<?php print $container_class; ?>">
-    <div class="navbar-header">
-      <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left"
-        href="<?php print $front_page; ?>"
-        title="<?php print t('Home'); ?>"> <img
-        src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
-      </a>
-      <a class="logo navbar-btn pull-left" target="_blank"
-        href="https://www.unige.ch/sciences/astro/en/"
-        title="Departement of Astronomy - university of Geneva"> <img
-        src="<?php print $directory;?>/logo-fac-sciences.png" alt="Departement of Astronomy - university of Geneva" />
-      </a>
-      <a class="logo navbar-btn pull-left" target="_blank"
-        href="https://www.isdc.unige.ch/integral/"
-        title="The INTErnational Gamma-Ray Astrophysics Laboratory - INTEGRAL"> <img
-        src="<?php print $directory;?>/logo-isdc.png" alt="The INTErnational Gamma-Ray Astrophysics Laboratory - INTEGRAL" />
-      </a>
-      <a class="logo navbar-btn pull-left"
-        href="https://www.epfl.ch/labs/lastro" target="_blank"
-        title="Laboratory of Astrophysics (LASTRO) - EPFL"> <img
-        src="<?php print $directory;?>/logo-epfl.png" alt="Laboratory of Astrophysics (LASTRO) - EPFL" />
-      </a>
-      <a class="logo navbar-btn pull-left"
-        href="https://apc.u-paris.fr/APC_CS/en" target="_blank"
-        title="Laboratoire AstroParticule et Cosmologie (APC)"> <img
-        src="<?php print $directory;?>/logo-apc.png" alt="Laboratoire AstroParticule et Cosmologie (APC)" />
-      </a>
-      <?php endif; ?>
-
-      <?php if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php print $front_page; ?>"
-        title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
-      <?php endif; ?>
-
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <button type="button" class="navbar-toggle"
-        data-toggle="collapse" data-target="#navbar-collapse">
-        <span class="sr-only"><?php print t('Toggle navigation'); ?></span>
-        <span class="icon-bar"></span> <span class="icon-bar"></span> <span
-          class="icon-bar"></span>
-      </button>
-      <?php endif; ?>
-    </div>
-
-    <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-      <div class="navbar-collapse collapse" id="navbar-collapse">
-      <nav role="navigation">
-          <?php if (!empty($primary_nav)): ?>
-            <?php print render($primary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($secondary_nav)): ?>
-            <?php print render($secondary_nav); ?>
-          <?php endif; ?>
-          <?php if (!empty($page['navigation'])): ?>
-            <?php print render($page['navigation']); ?>
-          <?php endif; ?>
-        </nav>
-    </div>
-    <?php endif; ?>
-  </div>
-</header>
 
 <div class="main-container <?php print $container_class; ?>">
 
-  <header role="banner" id="page-header">
-    <?php if (!empty($site_slogan)): ?>
-      <p class="lead"><?php print $site_slogan; ?></p>
-    <?php endif; ?>
-
-    <?php print render($page['header']); ?>
-  </header>
-  <!-- /#page-header -->
-
   <div class="row">
 
-
-    <section <?php print $content_column_class; ?>>
-      <?php if (!empty($page['highlighted'])): ?>
-        <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
-      <?php endif; ?>
-      <?php
-
-      if (! empty($breadcrumb)) :
-        print $breadcrumb;
-      endif;
-
-      ?>
+    <section<?php print $content_column_class; ?>>
       <a id="main-content"></a>
       <?php print $messages; ?>
       <?php if (!empty($tabs)): ?>
