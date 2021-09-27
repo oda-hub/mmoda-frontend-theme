2aa7a3ac63d6661766f757e922691e21f2e10d86
Mohamed Meharga <mohamed.meharga@unige.ch>
2021-09-27 10:15:25 +0200

    Updated emails rendering

--- template.php
+++ template.php
@@ -4,6 +4,16 @@
  * bootstrap_astrooda_form_alter(&$form, &$form_state, $form_id) { ... }
  */
 
+function bootstrap_astrooda_form_alter(&$form, &$form_state, $form_id)
+{
+  switch ($form_id) {
+    case 'user_profile_form':
+      error_log("inuser_profile_form:\n".print_r($form, true) );
+      unset($form['contact']);
+      unset($form['mimemail']);
+      break;
+  }
+}
 
 function bootstrap_astrooda_form_element($variables) {
   $element =& $variables['element'];
